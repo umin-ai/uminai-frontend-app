8038a8adf22b3a06c8a9ec9818b809a9bd9ef5fe
mlyk1234 <32647481+morgan-redbite@users.noreply.github.com>
2025-03-05 18:15:41 +0800

    Update consts.ts

--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -1,5 +1,5 @@
-export const BASE_SERVER_URL = 'http://localhost:8000';
-// export const BASE_SERVER_URL = 'https://big-tiffi-selftagger-9164a6f9.koyeb.app';
+// export const BASE_SERVER_URL = 'http://localhost:8000';
+export const BASE_SERVER_URL = 'https://big-tiffi-selftagger-9164a6f9.koyeb.app';
 
 export interface GeneralState {
   isSuccess: boolean;

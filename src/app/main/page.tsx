5d672756abfb305b9071ed2bdf3c34c8ced2341e
mlyk1234 <32647481+morgan-redbite@users.noreply.github.com>
2025-03-28 03:57:36 +0800

    Update

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -3,6 +3,7 @@ import { FlexColumn, FlexRow } from "@ap/components/general";
 import { Text } from "@mantine/core";
 import Image from "next/image";
 import styled from "styled-components";
+import { ItemIcons } from "/public/svgs";
 
 const CardXL = styled.div`
   width: 100%;
@@ -32,24 +33,14 @@ const Card = styled.div`
 `;
 
 const Sidepanel = styled(FlexColumn)`
-  background: rgb(38,20,84);
-  background: linear-gradient(180deg, rgba(38,20,84,1) 0%, rgba(101,80,154,1) 25%, rgba(139,121,93,0.9752275910364145) 63%, rgba(120,120,186,1) 100%);
-
+  background: rgb(53,59,113);
+  background: linear-gradient(90deg, rgba(53,59,113,1) 0%, rgba(45,51,98,1) 25%, rgba(40,46,89,1) 63%, rgba(35,40,80,1) 100%);
   border-radius: 16px;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.5);
   height: 100%;
   padding: 20px;
+  align-items: center;
 `;
-
-const TabItem = styled(FlexRow)<{ active?: boolean }>`
-  gap: 8px;
-  padding: 8px 16px;
-  border-radius: 8px;
-  cursor: pointer;
-  background-color: ${({ active }: { active?: boolean }) => active ? '#F2F2F2' : 'transparent'};
-  color: ${({ active }: { active?: boolean }) => active ? '#000' : '#666'};
-  font-weight: ${({ active }: { active?: boolean }) => active ? 'bold' : 'normal'};
-`
 
 const ContentLayout = styled(FlexRow)`
   gap: 56px;
@@ -58,25 +49,62 @@ const ContentLayout = styled(FlexRow)`
 
 const Dashboard = styled(FlexRow)`
   height: 100vh;
-  padding: 8px 0px;
+  padding: 16px 16px;
   gap: 20px;
 `;
 
 const MainImg = styled(Image)``;
 
+const TabItem = styled(FlexRow)<{ active?: boolean }>`
+  align-items: center;
+  gap: 8px;
+  padding: 8px 32px 8px 16px;
+
+  border-radius: 0px 8px 8px 0;
+  cursor: pointer;
+
+  border-left: 5px solid #fff;
+  background-color: rgba(255, 255, 255, 0.3); // White with 30% opacity
+  backdrop-filter: blur(10px); // Adjust blur radius as needed
+  color: ${({ active }: { active?: boolean }) => active ? '#000' : '#fff'};
+  font-weight: ${({ active }: { active?: boolean }) => active ? 'bold' : 'normal'};
+
+  svg, path {
+    width: 24px !important;
+    height: 24px !important;
+    stroke: #fff !important;
+  }
+`
+
+const TabRoot = styled(FlexColumn)`
+  gap: 16px;
+`;
+
 export default function Main() {
   return (
     <Dashboard>
       <Sidepanel>
-        <MainImg src={'/images/uminai.png'} alt='uminai' width={128} height={0}/>
-        <TabItem>Listing</TabItem>
-        <TabItem>Setting</TabItem>
+        <MainImg src={'/images/uminai.png'} alt='uminai' width={80} height={0}/>
+        <TabRoot className="mt-5">
+          <TabItem>
+            <ItemIcons.Home />
+            Dashboard
+          </TabItem>
+          <TabItem>
+            <ItemIcons.AI />
+            Agent
+          </TabItem>
+          <TabItem>
+            <ItemIcons.Setting />
+            Settings
+          </TabItem>
+        </TabRoot>
       </Sidepanel>
       <CardXL>
         <ContentLayout>
           <FlexColumn className="gap-[56px]">
             <Card>
-              some
+              Dashboard
             </Card>
             <Card>
               some
